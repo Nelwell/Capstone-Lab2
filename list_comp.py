0262ca4316123534d6987539ee0431fa94784308
Nelwell <nick.elwell206@gmail.com>
2021-01-20 03:26:10 -0600

    Lab 2 exercises complete and commented in author_class.py, author_class_no_dups.py, and student_dataclass.py files.

--- list_comp.py
+++ list_comp.py
@@ -10,3 +10,19 @@ print(only_itec)
 
 # Record temperatures ever day. Record -1 if not possible to take measurement.
 high_temps = [-1, 78, 72, 67, -1, 51, 87, 82, -1, 54, 67, 78, -1, 70]
+
+# make each number in the list +1 higher
+numbers = [2, 4, 6]
+
+plus_one_numbers = [plus_one + 1 for plus_one in numbers]
+print(plus_one_numbers)
+
+numbers = [0, 2, 4, 0, 22, 1]
+
+# removes all zeros from the list of numbers
+no_zeros = [not_zero for not_zero in numbers if not_zero != 0]
+print(no_zeros)
+
+numbers_list = [0, 10, 4, 0, 32]
+non_zeros_doubled = [n*2 for n in numbers_list if n != 0]
+print(non_zeros_doubled)
